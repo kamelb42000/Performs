d3a73eea1d1b8ab95c7dd4b61e506308dc1ccd10
kamelb42000 <bbs42000@hotmail.fr>
2023-07-06 10:10:28 +0200

    ajout entrainement

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -3,4 +3,24 @@ class SportsController < ApplicationController
   def index
     @sports = current_user.sports
   end
+
+  def new
+    @sport = Sport.new
+  end
+
+  def create
+    @sport = Sport.new(sport_params)
+    @sport.user = current_user
+    if @sport.save
+      redirect_to sports_path, flash: { success: "Le sport a été créé avec succès" }
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def sport_params
+    params.require(:sport).permit(:name, :teams, :sports)
+  end
 end
